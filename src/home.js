2a6f95896dede90fc6e992d0765589977f36a598
dymjin <dymjin@gmail.com>
2023-12-11 10:32:07 +0200

    Tweak styling* *remove cottage img from main page, change tabs styling, fixed footer to bottom of page

--- src/home.js
+++ src/home.js
@@ -1,12 +1,12 @@
 function home() {
     const img = document.createElement('img');
-    img.classList.add('main-img');
-    img.setAttribute("src", "../imgs/cottage.jpg");
+    // img.classList.add('main-img');
+    // img.setAttribute("src", "../imgs/cottage.jpg");
     const p_main = document.createElement('p');
     p_main.classList.add('p-main');
     p_main.textContent = "Queen's cottage is a wonderful place to keep cozy and enjoy some wholesome food.";
     const contentBox = document.querySelector('#contentBox');
-    contentBox.appendChild(img);
+    // contentBox.appendChild(img);
     contentBox.appendChild(p_main);
 }
 
